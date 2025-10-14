c7f36c94bc450aa849ef0064e15918679973175f
lorenzopalaia <lorenzopalaia53@gmail.com>
2025-10-14 13:20:21 +0200

    ✨ feat: update sitemap frequencies for specific paths

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,4 +4,21 @@ module.exports = {
   siteUrl: "https://euro-hackathons.vercel.app",
   generateRobotsTxt: true,
   priority: 1,
+  transform: async (config, path) => {
+    if (path === "/docs" || path === "/privacy" || path === "/terms") {
+      return {
+        loc: path,
+        changefreq: "yearly",
+        priority: 0.8,
+        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      };
+    }
+
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+    };
+  },
 };
